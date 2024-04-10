67b0072b5cfca62c68482af605c95c92999f1d29
Joel <102653084+Joelt112@users.noreply.github.com>
2024-04-10 16:22:36 +0530

    svg uploaded

--- src/footer.jsx
+++ src/footer.jsx
@@ -9,9 +9,9 @@ function Ooter() {
     <div className='content'>
       <h1>EKA</h1>
       <p>its fun you know</p>
-    <div>
-      <a href='#'></a>
-      <a href='#'></a>
+    <div className='cont1'>
+    <svg height="24" preserveAspectRatio="xMinYMin" viewBox="-2 -2 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m8.695 6.937v1.377h-1.008v1.683h1.008v5.003h2.072v-5.003h1.39s.131-.807.194-1.69h-1.576v-1.15c0-.173.226-.404.45-.404h1.128v-1.753h-1.535c-2.174 0-2.123 1.685-2.123 1.937z"/><path d="m10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0 2c-5.523 0-10-4.477-10-10s4.477-10 10-10 10 4.477 10 10-4.477 10-10 10z"/></svg>
+    <svg height="24" width="24" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg"><path d="m46.7845 12.6136h-21.569c-6.9598 0-12.6019 5.6421-12.6019 12.6019v21.5689c0 6.9598 5.6421 12.6019 12.6019 12.6019h21.5689c6.9598 0 12.6019-5.6421 12.6019-12.6019v-21.5689c.0001-6.9598-5.642-12.6019-12.6018-12.6019z" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/><circle cx="36" cy="36" fill="none" r="11.29" stroke="#000" stroke-miterlimit="10" stroke-width="2"/><circle cx="49.7092" cy="22.2908" r="2.4193"/></svg>
       <a href='#'></a>
       <a href='#'></a>
     </div>
