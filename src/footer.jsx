fede6cd55e7e56829e5b0c148a0f0b4ac8dcb1f0
Joel <102653084+Joelt112@users.noreply.github.com>
2024-04-16 08:21:25 +0530

    home il idan enthuvann ariyilla

--- src/footer.jsx
+++ src/footer.jsx
@@ -11,23 +11,34 @@ function Ooter() {
     <h3>College of Engineering Pathanapuram</h3>
       
     <div className='cont1'>
-    <svg height="24" preserveAspectRatio="xMinYMin" viewBox="-2 -2 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m8.695 6.937v1.377h-1.008v1.683h1.008v5.003h2.072v-5.003h1.39s.131-.807.194-1.69h-1.576v-1.15c0-.173.226-.404.45-.404h1.128v-1.753h-1.535c-2.174 0-2.123 1.685-2.123 1.937z"/><path d="m10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0 2c-5.523 0-10-4.477-10-10s4.477-10 10-10 10 4.477 10 10-4.477 10-10 10z"/></svg>
-    <svg height="24" width="24" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg"><path d="m46.7845 12.6136h-21.569c-6.9598 0-12.6019 5.6421-12.6019 12.6019v21.5689c0 6.9598 5.6421 12.6019 12.6019 12.6019h21.5689c6.9598 0 12.6019-5.6421 12.6019-12.6019v-21.5689c.0001-6.9598-5.642-12.6019-12.6018-12.6019z" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/><circle cx="36" cy="36" fill="none" r="11.29" stroke="#000" stroke-miterlimit="10" stroke-width="2"/><circle cx="49.7092" cy="22.2908" r="2.4193"/></svg>
-    <svg enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m0 0h24v24h-24z" fill="none"/><path d="m12 2c-3.87 0-7 3.13-7 7 0 1.74.5 3.37 1.41 4.84.95 1.54 2.2 2.86 3.16 4.4.47.75.81 1.45 1.17 2.26.26.55.47 1.5 1.26 1.5.79 0 1-.95 1.25-1.5.37-.81.7-1.51 1.17-2.26.96-1.53 2.21-2.85 3.16-4.4.92-1.47 1.42-3.1 1.42-4.84 0-3.87-3.13-7-7-7zm0 9.75c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>
+    <svg className='svg' height="24" preserveAspectRatio="xMinYMin" viewBox="-2 -2 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m8.695 6.937v1.377h-1.008v1.683h1.008v5.003h2.072v-5.003h1.39s.131-.807.194-1.69h-1.576v-1.15c0-.173.226-.404.45-.404h1.128v-1.753h-1.535c-2.174 0-2.123 1.685-2.123 1.937z"/><path d="m10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0 2c-5.523 0-10-4.477-10-10s4.477-10 10-10 10 4.477 10 10-4.477 10-10 10z"/></svg>
+    <a href='https://www.instagram.com/eka_techfest?igsh=M2RiYmt6Y2dicXln'><svg className='svg' height="24" width="24" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg"><path d="m46.7845 12.6136h-21.569c-6.9598 0-12.6019 5.6421-12.6019 12.6019v21.5689c0 6.9598 5.6421 12.6019 12.6019 12.6019h21.5689c6.9598 0 12.6019-5.6421 12.6019-12.6019v-21.5689c.0001-6.9598-5.642-12.6019-12.6018-12.6019z" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/><circle cx="36" cy="36" fill="none" r="11.29" stroke="#000" stroke-miterlimit="10" stroke-width="2"/><circle cx="49.7092" cy="22.2908" r="2.4193"/></svg></a>
+    <a href='https://maps.app.goo.gl/pZ9kkwUduGQnDSsc6'><svg className='svg' enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m0 0h24v24h-24z" fill="none"/><path d="m12 2c-3.87 0-7 3.13-7 7 0 1.74.5 3.37 1.41 4.84.95 1.54 2.2 2.86 3.16 4.4.47.75.81 1.45 1.17 2.26.26.55.47 1.5 1.26 1.5.79 0 1-.95 1.25-1.5.37-.81.7-1.51 1.17-2.26.96-1.53 2.21-2.85 3.16-4.4.92-1.47 1.42-3.1 1.42-4.84 0-3.87-3.13-7-7-7zm0 9.75c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg></a>
       <a href='#'></a>
     </div>
     </div>
     <div className='foot-cont'>
     <h4>Home</h4>
-    
+    <li>
+  <a href="#"> <br />
+  
+  </a>
+ </li>
     </div>
     <div className='foot-cont'>
     <h4>About us</h4>
-   
+    <li>
+  <a href="#">College Of Engineering Pathnapuram <br />
+  Under Cape | Govt Of kerala
+  </a>
+ </li>
     </div>
     <div className='foot-cont'>
     <h4>contact us</h4>
-    
+ <li>
+  <a href="#">+91 8606487641
+  </a>
+ </li>
     </div>
 
     </div>
